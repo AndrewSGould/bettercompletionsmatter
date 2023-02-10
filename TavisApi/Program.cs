55d69deedf9f8a005af9e7ad58774e2464190632
Andrew Gould <andrew.scott.gould@gmail.com>
2023-02-10 15:33:50 -0500

    readd signal r

--- TavisApi/Program.cs
+++ TavisApi/Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// builder.Services.AddSignalR();
+builder.Services.AddSignalR();
 
 // builder.Services.AddCors(options =>
 // {
