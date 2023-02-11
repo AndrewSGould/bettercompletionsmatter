c0eade682e0a9930b8535026e31241c228d7f428
Andrew Gould <andrew.scott.gould@gmail.com>
2023-02-10 19:39:48 -0500

    small refactor of magic id

--- TavisApi/Services/BcmService.cs
+++ TavisApi/Services/BcmService.cs
@@ -6,17 +6,23 @@ namespace TavisApi.Services;
 public class BcmService : IBcmService 
 {
   private TavisContext _context;
+  private int _bcmContestId;
 
   public BcmService(TavisContext context) {
     _context = context;
+    _bcmContestId = GetContestId();
   }
 
   public List<Player> GetPlayers() {
-    var bcmPlayers = _context.PlayerContests!.Where(x => x.ContestId == 1).Select(x => x.PlayerId);
+    var bcmPlayers = _context.PlayerContests!.Where(x => x.ContestId == _bcmContestId).Select(x => x.PlayerId);
     return _context.Players!.Where(x => x.IsActive && bcmPlayers.Contains(x.Id)).OrderBy(x => x.Name).ToList();
   }
 
   public DateTime? GetContestStartDate() {
-    return _context.Contests.Where(x => x.Id == 1).Select(x => x.StartDate).FirstOrDefault();
+    return _context.Contests.Where(x => x.Id == _bcmContestId).Select(x => x.StartDate).FirstOrDefault();
+  }
+
+  private int GetContestId() {
+    return _context.Contests.Where(x => x.Name.Contains("Better Completions Matter")).First().Id;
   }
 }
